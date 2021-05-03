9689e5829daec1975070164cbc743e1074651112
Botck <torck@tuta.io>
2021-05-04 00:12:33 +0800

    add bridge file

---
+++ Brook/Brook/Modules/Views/BKDisplayServerInfoCell.swift
@@ -0,0 +1,19 @@
+//
+//  BKDisplayServerInfoCell.swift
+//  Brook
+//
+//  Created by Ghost on 5/3/21.
+//  Copyright © 2021 Brook. All rights reserved.
+//
+
+import Cocoa
+
+class BKDisplayServerInfoCell: NSTableCellView {
+
+    override func draw(_ dirtyRect: NSRect) {
+        super.draw(dirtyRect)
+
+        // Drawing code here.
+    }
+    
+}
